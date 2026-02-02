b622c3df8e7a1b4853dada8df306750e816be6a7
Gabriel Lingnau <gabriellingnau@MacBook-Air-de-Gabriel.local>
2026-02-02 14:32:18 -0400

    Criando as outras metadatas

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,4 +1,5 @@
 import { allPosts } from "contentlayer/generated";
+import { Metadata } from "next";
 import { notFound } from "next/navigation";
 
 import PostPage from "@/pages/blog-page/[slug]";
@@ -9,9 +10,46 @@ type BlogPostPageProps = {
   }>;
 };
 
-export const revalidate = 60
+export async function generateMetadata({
+  params,
+}: BlogPostPageProps): Promise<Metadata> {
+  const { slug } = await params;
 
+  const post = allPosts.find((p) => p.slug === slug);
 
+  if (!post) {
+    return {
+      title: "Post não encontrado | SiteBlog",
+      description: "Conteúdo não disponível.",
+    };
+  }
+
+  const imageUrl = post.image ?? "/images/default-og.jpg";
+
+  return {
+    title: post.title,
+    description: post.description,
+    authors: [{ name: post.author.name }],
+
+    openGraph: {
+      images: [
+        {
+          url: imageUrl,
+          width: 1200,
+          height: 630,
+          alt: post.title,
+        },
+      ],
+    },
+
+    twitter: {
+      card: "summary_large_image",
+      images: [imageUrl],
+    },
+  };
+}
+
+export const revalidate = 60;
 
 export async function generateStaticParams() {
   return allPosts.map((post) => ({
--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,6 +1,19 @@
 import { allPosts } from "contentlayer/generated";
+import type { Metadata } from "next";
 
 import { BlogList } from "@/templates/blog";
+
+export const metadata: Metadata = {
+  title: "Blog",
+  description:
+    "Dicas, tutoriais e estratégias práticas para impulsionar seu negócio digital.",
+
+  openGraph: {
+    title: "Blog — Dicas para Criar e Escalar Projetos Digitais",
+    description:
+      "Conteúdos sobre desenvolvimento, negócios e estratégias para crescer no digital.",
+  },
+};
 
 export default function BlogListPage() {
   const sortedPosts = allPosts.sort(
